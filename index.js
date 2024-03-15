a2af02ab9da76db387759c058977e6ca626ef329
Prathisz <prathish0071@gmail.com>
2024-03-15 18:30:59 +0530

    json file updated

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-import { Express } from "express";
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+const app = express()
+dotenv.config()
+const connect = async() =>{
+
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to db")
+    } catch (error) {
+        console.log(error)
+        
+    }
+};
+mongoose.connection.on("disconnected", ()=>{
+    console.log("mongoose disconnected!")
+})
+
+app.get("/user" , (req,res)=>{
+    res.send("Hello ")
+}) 
+
+
+
+
+app.listen(8500 , ()=>{
+    connect()
+    console.log("Connected to backend!!")
+})
